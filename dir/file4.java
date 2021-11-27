3bd93245a243545ffd05eb7af3bc734c2c2181e3
ymstry <91677614+ymstry@users.noreply.github.com>
2021-11-27 18:53:50 +0900

    newです

---
+++ dir/file4.java
@@ -0,0 +1,5 @@
+public class file4(){
+	public static void main() {
+		System.out.println("d");
+	}
+}
---
+++ dir/file5.java
@@ -0,0 +1,5 @@
+public class file5(){
+	public static void main() {
+		System.out.println("e");
+	}
+}
